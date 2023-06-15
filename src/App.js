11d80065d3b704e6c640554488acfe20fac73846
이진아 <mnb2098@naver.com>
2023-06-15 13:18:40 +0900

    두 번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
+  const [colorCode, setColorCode] = useState("Purple");
 
+  const changeText = (e) => {
+    setColorCode(e.target.value);
+  }
+
+  const activeEnter = (e) => {
+    if(e.key === "Enter") {
+      applyColor();
+    }
+  }
+  const applyColor = () => {
+    document.documentElement.style.setProperty(
+      '--background-color', 
+      colorCode
+    );
+  }
+
+  return (
+    <div style={{margin: "50px 50px"}} >
+      <label>Background Color: </label>
+      <p>{colorCode}</p>
+      <input value={colorCode} onChange={changeText} onKeyDown={(e) => activeEnter(e)}/>
+      {/* <button onClick={applyColor}>적용</button> */}
     </div>
   );
 }
